1579b98dcdacdec3477857bab1c0c9e64a5b117f
jD91mZM2 <me@krake.one>
2019-07-07 11:55:36 +0200

    Add floating-point tests

--- src/f80.rs
+++ src/f80.rs
@@ -32,3 +32,16 @@ pub fn f80_to_f64(mut input: u128) -> f64 {
 
     f64::from_bits(output)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        eprintln!("sqrt(64) = {}", f80_to_f64(302277571763841567555584));
+        assert!((f80_to_f64(302277571763841567555584) - 8.0).abs() < std::f64::EPSILON);
+        eprintln!("sqrt(32) = {}", f80_to_f64(302262945465556336010372));
+        assert!((f80_to_f64(302262945465556336010372) - 5.65685424949238).abs() < std::f64::EPSILON);
+    }
+}
